5adedd56d1b90958459c9381b2aa5e7e9c4ebb0f
Khadijah Jennings <kjennings855@gmail.com>
2019-12-13 06:18:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+
+  badges = []
+  attendees.each do |x| 
+  badges << badge_maker(x)
+  end
+    badges
+  end
+ 
+ 
+def assign_rooms(attendees)
+  room_assignments = []
+  counter = 1
+  attendees.each do |first_name|
+  room_assignments << ("Hello, #{first_name}! You'll be assigned to room #{counter}!")
+  counter += 1
+  
+end
+room_assignments
+end
